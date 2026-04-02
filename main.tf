74f7e650ae021495bdb63e03be3f19632a72e879
Pavlo <pavlo.rohozhyn@gmail.com>
2026-04-02 11:10:52 +0200

    Initial commit

---
+++ main.tf
@@ -0,0 +1,23 @@
+provider "aws" {
+    region = "us-east-1"
+}
+
+resource "aws_vpc" "main" {
+    cidr_block = "10.0.0.0/16"
+}
+
+resource "aws_subnet" "main" {
+    vpc_id = aws_vpc.main.id
+    cidr_block = "10.0.1.0/24"
+    availability_zone = "us-east-1a"
+}
+
+resource "aws_instance" "example" {
+    ami = "ami-0ec10929233384c7f"
+    instance_type = "t3.micro"
+    subnet_id = aws_subnet.main.id
+
+    tags = {
+        Name = "terra-test-instance"
+    }
+}
